cd66e8877b85ed059984ca8ea52fbb2f1b29e28c
commitsovercoffee <commitsovercoffee@gmail.com>
2023-09-03 03:06:19 +0530

    adds text to landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,36 @@
-<h1 class="font-bold border-2 rounded-xl">Welcome to SvelteKit</h1>
-<p>
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script>
+	import A from "./components/A.svelte";
+</script>
+
+<article class="mt-32 selection:bg-teal-300">
+	<h1>Bold, Attention stealing sentence.</h1>
+
+	<p>
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque,
+		id maiores cum inventore, non vel dolore ipsum facere, animi
+		vero velit! Incidunt delectus aspernatur similique sit. Ea
+		delectus quo dignissimos.
+	</p>
+
+	<ul>
+		<li>Interesting bullet point and an <A />.</li>
+		<li>A <A /> and another interesting bullet point.</li>
+		<li>
+			Interesting bullet point <A />.
+		</li>
+	</ul>
+
+	<div class="mt-32 flex justify-center">
+		<div
+			class="flex -z-10 justify-center items-center relative left-6 rounded-full bg-teal-300 w-60 h-60 opacity-60"
+		>
+			<span class="z-10 text-slate-800"> Design </span>
+		</div>
+
+		<div
+			class="flex -z-10 justify-center items-center relative right-6 rounded-full bg-amber-300 w-60 h-60 opacity-60"
+		>
+			<span class="z-10 text-slate-800"> Development </span>
+		</div>
+	</div>
+</article>
---
+++ src/routes/components/A.svelte
@@ -0,0 +1,23 @@
+<script>
+	/**
+	 * The label text for the link.
+	 *
+	 * @type {string}
+	 * @default "[insert url label]"
+	 */
+	export let label = "[insert link]";
+
+	/**
+	 * The URL to navigate to when the link is clicked.
+	 *
+	 * @type {string}
+	 * @default "/"
+	 */
+	export let link = "/";
+</script>
+
+<a
+	class="hover:bg-teal-300 transition-all duration-200 underline-offset-4"
+	href={link}
+	>{label}
+</a>
