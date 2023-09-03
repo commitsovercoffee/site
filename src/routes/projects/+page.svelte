ef1d4b32a53683819b7cc2259a3688edc20a6a2f
commitsovercoffee <commitsovercoffee@gmail.com>
2023-09-03 19:04:37 +0530

    adds projects

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,71 @@
+<script>
+	import Title from "../components/Title.svelte";
+
+	let projects = [
+		{
+			name: "Project 1",
+			desc: "Description for Project 1",
+			repo: "https://example.com/project1",
+			demo: "/",
+		},
+		{
+			name: "Project 1",
+			desc: "Description for Project 1",
+			repo: "https://example.com/project1",
+			demo: "",
+		},
+		{
+			name: "Project 1",
+			desc: "Description for Project 1",
+			repo: "https://example.com/project1",
+			demo: "",
+		},
+		{
+			name: "Project 1",
+			desc: "Description for Project 1",
+			repo: "https://example.com/project1",
+			demo: "",
+		},
+	];
+</script>
+
+<section class="py-4">
+	<Title>Projects</Title>
+
+	<p class="my-8">
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque,
+		id maiores cum inventore, non vel dolore ipsum facere, animi
+		vero velit! Incidunt delectus aspernatur similique sit. Ea
+		delectus quo dignissimos.
+	</p>
+
+	<div class="flex flex-wrap justify-center">
+		{#each projects as project}
+			<div class="p-4 m-2 rounded-xl shadow-md bg-slate-100">
+				<p>
+					<a
+						href="/"
+						class="p-2 underline decoration-2 underline-offset-8 decoration-transparent hover:decoration-slate-800 transition duration-300 delay-50 ease-in"
+					>
+						{project.name}
+					</a>
+				</p>
+				<p>
+					{project.desc}
+				</p>
+				<a
+					href={`https://github.com/commitsovercoffee/${project.repo}`}
+					class="px-2 py-1 bg-zinc-100 border-2 border-zinc-200 rounded-xl hover:border-zinc-900 no-underline transition duration-300 ease-in"
+					>Source</a
+				>
+				{#if project.demo === ""}{:else}
+					<a
+						href={`https://github.com/commitsovercoffee/${project.demo}`}
+						class="px-2 py-1 bg-zinc-100 border-2 border-zinc-200 rounded-xl hover:border-zinc-900 no-underline transition duration-300 ease-in"
+						>Demo</a
+					>
+				{/if}
+			</div>
+		{/each}
+	</div>
+</section>
