5aceb471c7d6f2658613db25f5b3c5aef0a29001
commitsovercoffee <commitsovercoffee@gmail.com>
2023-09-03 06:48:14 +0530

    adds skills

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,9 +11,11 @@
 			desc: "Understand what makes a good engineer.",
 		},
 	];
+
+	let skills = ["bash", "git", "html", "css", "js", "go", "c", "python"];
 </script>
 
-<article class="mt-32 selection:bg-teal-300">
+<article class="my-32 selection:bg-teal-300">
 	<h1>Bold, Attention stealing sentence.</h1>
 
 	<p>
@@ -23,13 +25,20 @@
 		delectus quo dignissimos.
 	</p>
 
-	<ul>
+	<ul class="my-16">
 		<li>Interesting bullet point and an <A />.</li>
 		<li>A <A /> and another interesting bullet point.</li>
 		<li>
 			Interesting bullet point <A />.
 		</li>
 	</ul>
+
+	<p>
+		Find me on <A label="GitHub" />, <A label="Mastodon" />, <A
+			label="Twitter"
+		/> and <A label="YouTube" />. If you enjoy my works, consider
+		sponsoring me on GitHub Sponsor to keep them sustainable.
+	</p>
 
 	<svg class="my-32 p-2 w-full h-auto" viewBox="100 0 400 200">
 		<circle
@@ -72,20 +81,35 @@
 		delectus quo dignissimos.
 	</p>
 
-	<section>
+	<section class="my-32">
+		<h2>Skills</h2>
+		<div class="flex flex-wrap items-center p-2 m-6 rounded-xl">
+			{#each skills as skill}
+				<a href="/" class="hover:grayscale">
+					<img
+						class="m-2 rounded-xl"
+						src="https://picsum.photos/120"
+						alt=""
+					/>
+				</a>
+			{/each}
+		</div>
+	</section>
+
+	<section class="my-32">
 		<h2>Featured Blogs</h2>
 		{#each featuredBlogs as blog}
 			<div class="flex p-2 m-6 rounded-xl bg-slate-200">
 				<img
-					class="m-2 rounded-xl"
+					class="m-4 rounded-xl"
 					src="https://picsum.photos/200"
 					alt=""
 				/>
-				<div class="m-2 w-full">
+				<div class="m-2 p-4 w-full">
 					<p class="my-0 text-2xl font-black">
 						{blog.title}
 					</p>
-					<p class="my-0 text-base">
+					<p class="mt-4 text-base">
 						{blog.desc}
 					</p>
 				</div>
