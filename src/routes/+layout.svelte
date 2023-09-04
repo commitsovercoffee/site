abaf9a307a8f89d730acc0fe4badd4764d2c087b
commitsovercoffee <commitsovercoffee@gmail.com>
2023-09-04 12:29:42 +0530

    fixes cursor blob

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,24 +5,25 @@
 	import { spring } from "svelte/motion";
 
 	let coords = spring(
-		{ x: 50, y: 50 },
+		{ x: -500, y: 50 },
 		{
 			stiffness: 0.1,
 			damping: 0.4,
 		}
 	);
 
-	let size = spring(40);
+	let size = spring(20);
 </script>
 
-<svg
-	class="fixed w-full h-full top-0 left-0 z-50"
+<svelte:window
 	on:mousemove={(e) => {
 		coords.set({ x: e.clientX, y: e.clientY });
 	}}
-	on:mousedown={() => size.set(30)}
-	on:mouseup={() => size.set(10)}
->
+	on:mousedown={() => size.set(40)}
+	on:mouseup={() => size.set(20)}
+/>
+
+<svg class="fixed w-full h-full top-0 left-0 z-10 pointer-events-none">
 	<circle
 		cx={$coords.x}
 		cy={$coords.y}
